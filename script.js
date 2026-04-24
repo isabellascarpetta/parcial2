c3cca5e1a85bb4d2fe62f5e8fb205346b147d36c
Isabella Scarpetta Escobar <est.isabella.scarp@unimilitar.edu.co>
2026-04-24 09:50:09 -0500

    En el archivo script.js se implementa la function drawpixel y function bresenhamLine para empezar a colocar los puntos de cada punto que va a formar la linea de la circunferencia.

---
+++ script.js
@@ -0,0 +1,49 @@
+// Función de apoyo para dibujar un píxel individual
+function drawPixel(ctx, x, y, color = "#000000") {
+    ctx.fillStyle = color;
+    ctx.fillRect(Math.floor(x), Math.floor(y), 1, 1);
+}
+
+/**
+ * Implementación del algoritmo de Bresenham para líneas.
+ * @param {number} x0, y0 - Coordenadas iniciales
+ * @param {number} x1, y1 - Coordenadas finales
+ * @returns {void}
+ */
+function bresenhamLine(x0, y0, x1, y1, color) {
+    //Se coloca para poner los puntos
+    let dx = Math.abs(x1 - x0);
+    let dy = Math.abs(y1 - y0);
+    let sx = (x0 < x1) ? 1 : -1;
+    let sy = (y0 < y1) ? 1 : -1;
+    let err = dx - dy;
+
+    while (true) {
+        drawPixel(ctx, x0, y0, color);
+
+        if (x0 === x1 && y0 === y1) break;
+        
+        let e2 = 2 * err;
+        //Aquí se calcula el parámetro de decisión p, con la ecuación de bresenham
+        if (e2 > -dy) {
+            err -= dy;
+            x0 += sx;
+        }
+        if (e2 < dx) {
+            err += dx;
+            y0 += sy;
+        }
+    }
+}
+
+/**
+ * Calcula los vértices de un polígono regular.
+ * @param {number} centerX, centerY - Centro
+ * @param {number} sides - Número de lados
+ * @param {number} radius - Radio
+ * @returns {Array} Arreglo de objetos {x, y}
+ */
+function getPolygonVertices(centerX, centerY, sides, radius) {
+    // Desarrollo del estudiante (Uso de Math.sin/Math.cos y retorno de datos)
+
+}
